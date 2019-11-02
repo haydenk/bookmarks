7410c4f208ae36c49191c8ef9694bf4f95dd1a53
Hayden King <81236+haydenk@users.noreply.github.com>
2019-11-02 15:07:07 -0500

    refactor to convert bookmarks from db to csv and markdown

--- get_bookmarks.rb
+++ get_bookmarks.rb
@@ -1,21 +1,24 @@
 require 'sqlite3'
-require 'digest/sha2'
+require 'csv'
+require 'cgi'
 
 begin
   db = SQLite3::Database.new "bookmarks.db"
 
-  stm = db.prepare "SELECT id, url FROM bookmarks"
-  rs = stm.execute
+  File.open('README.md', 'wb') do |file|
+    file << "[CSV](bookmarks.csv)\n\n"
+    stm = db.prepare "SELECT title, url FROM bookmarks"
+    stm.execute.each do |row|
+      title, url = row
+      title = CGI.escape_html(title)
+      file << "1. [#{title}](#{url})\n"
+    end
+  end
 
-  rs.each do |row|
-    id, url = row
-    hash = Digest::SHA512.hexdigest(url)
-    puts "ID: #{id}"
-    puts "URL: #{url}"
-    puts "Hash: #{hash}"
-    puts
-    update_stm = db.prepare"UPDATE bookmarks SET hash_key = ? WHERE id = ?"
-    update_stm.execute hash, id
+  CSV.open('bookmarks.csv', 'wb') do |csv|
+    csv << %w(id title url)
+    stm = db.prepare "SELECT id, title, url FROM bookmarks"
+    stm.execute.each { |row| csv << row }
   end
 
 rescue  SQLite3::Exception => error
