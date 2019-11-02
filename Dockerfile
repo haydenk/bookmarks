afa19ae974902d0c9b838a9d7ab8e424c50429c6
Hayden King <81236+haydenk@users.noreply.github.com>
2019-11-02 18:13:16 -0500

    refactor action to root directory

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM ruby:2.6.5-alpine
 WORKDIR /usr/src/app
-COPY Gemfile ./
+COPY Gemfile entrypoint.sh ./
 RUN set -ex; \
     apk add build-base sqlite-dev; \
     gem install bundler; \
     bundle install
+ENTRYPOINT ["entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+bundle exec ruby get_bookmarks.rb
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"
+git add -f README.md bookmarks.csv
+git commit -m "Add changes" -a
+
+remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git push "${remote_repo}" HEAD:master --follow-tags $_FORCE_OPTION
