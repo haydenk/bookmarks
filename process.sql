83daafd6b2da9bbae2cac4ab68fc2b37934500f5
Hayden <haydenk@users.noreply.github.com>
2024-08-29 14:13:54 -0500

    update bookmarks

--- process.sql
+++ process.sql
@@ -6,17 +6,20 @@ create table bookmarks
     hash   CHAR(64) primary key
 );
 
-insert or replace into bookmarks (title, domain, url, hash)
-select title, domain, url, sha256(url) as 'hash'
+insert or replace into bookmarks (hash, title, domain, url)
+select distinct sha256(url) as 'hash', title, domain, url
 from read_csv('bookmarks.csv')
 where regexp_matches(url, '^https?\:\/\/');
 
-#insert or replace into bookmarks (title, domain, url, hash)
-#select title
-#     , regexp_replace(regexp_extract(url, '^https?\:\/\/([a-z0-9.-]+).*$', 1), '^www\.', '') as 'domain'
-#     , rtrim(regexp_replace(url, '(fbclid|pfb_.*|pd_.*|ref_|feature)=.*', ''), '?&') as 'url'
-#     , sha256(url) as 'hash'
-#from read_json('data/*.json')
-#where regexp_matches(url, '^https?\:\/\/');
+insert or replace into bookmarks (hash, title, domain, url)
+select sha256(url) as 'hash'
+     , title
+     , regexp_replace(regexp_extract(url, '^https?\:\/\/([a-z0-9.-]+).*$', 1), '^www\.', '') as 'domain'
+     , rtrim(regexp_replace(url, '(fbclid|pfb_.*|pd_.*|ref_|feature)=.*', ''), '?&') as 'url'
+from read_json('data/*.json')
+where regexp_matches(url, '^https?\:\/\/');
 
 copy (select title, domain, url from bookmarks order by domain asc) to 'bookmarks.csv';
+
+.mode markdown
+select title, domain, url from bookmarks order by domain asc
